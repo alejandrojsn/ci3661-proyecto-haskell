1703232fe648277f1c968cf81286cedddd1b0491
inojosacs <cristianinojosa@gmail.com>
2022-03-16 15:26:14 -0400

    parte 2

---
+++ AA.hs
@@ -0,0 +1,51 @@
+module AA (
+  AA(..),
+  empty,
+  isEmpty,
+  insert,
+  lookup,
+) where
+
+data AA k a = Empty
+            | Node { lvl :: Int
+            , key :: k
+            , val :: a
+            , lAA :: AA k a
+            , rAA :: AA k a
+            }
+            deriving Show
+
+mapTree :: (a -> b) -> AA k a -> AA k b
+mapTree f Empty = Empty
+mapTree f (Node l k v lt rt) = Node l k (f v) (mapTree f lt) (mapTree f rt)
+
+instance Functor (AA k) where
+  fmap = mapTree
+
+foldrA :: (a -> b -> b) -> b -> AA k a -> b
+foldrA f b Empty = b
+foldrA f b (Node _ _ v lt rt) = foldrA f (f v (foldrA f b rt)) lt
+
+instance Foldable (AA k) where
+  foldr = foldrA
+
+empty :: AA k a
+empty = Empty
+
+isEmpty :: AA k a -> Bool
+isEmpty Empty = True
+isEmpty _ = False
+
+insert :: (Ord k) => k -> a -> AA k a -> AA k a
+insert k v Empty = Node 0 k v Empty Empty
+insert k v (Node l k' v' lAA rAA)
+  | k == k' = Node l k' v' lAA rAA
+  | k < k' = Node l k' v' (insert k v lAA) rAA
+  | k > k' = Node l k' v' lAA (insert k v rAA)
+
+lookup' :: (Ord k) => k -> AA k a -> Maybe a
+lookup' k Empty = Nothing
+lookup' k (Node l k' v' lAA rAA)
+  | k == k' = Just v'
+  | k < k' = lookup' k lAA
+  | k > k' = lookup' k rAA
---
+++ Match.hs
@@ -0,0 +1,58 @@
+module Match(
+  Match(..),
+  Target(..),
+  Guess(..),
+  match,
+) where
+
+import Text.Read
+import Text.ParserCombinators.ReadPrec 
+
+data Match = Absent Char
+            | Misplaced Char
+            | Correct Char
+
+data Target = Target String
+data Guess = Guess String
+
+{- Hace mas de 1 pasada (acomodar) -}
+match :: Guess -> Target -> [Match]
+match (Guess xs) (Target ys) =  [getMatch x y | (x,y) <- zip xs ys]
+                                where getMatch x y
+                                        | x == y = Correct x
+                                        | x `elem` ys = Misplaced x
+                                        | otherwise = Absent x
+
+fullMatch :: [Match] -> Bool
+fullMatch = all (\x -> case x of
+                          Correct _ -> True
+                          _ -> False)
+
+instance Show Target where
+  show (Target xs) = "It was " ++ xs
+
+instance Show Guess where
+  show (Guess xs) = "Your guess " ++ xs
+
+instance Show Match where
+  show (Correct x) = "\129001 " ++ [x]
+  show (Misplaced x) = "\129000 " ++ [x]
+  show (Absent x) = "\11035 " ++ [x]
+
+instance Read Match where 
+  readPrec = parens $ do
+    c <- get
+    case c of
+      "\129001" -> do
+        _ <- get
+        x <- get
+        return $ Correct x
+      "\129000" -> do
+        _ <- get
+        x <- get
+        return $ Misplaced x
+      "\11035" -> do
+        _ <- get
+        x <- get
+        return $ Absent x
+      _ -> pfail
