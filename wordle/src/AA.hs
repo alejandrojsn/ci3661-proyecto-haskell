de27349f8dc33d7b75aa49e1b22e60ceefe59cd3
Alejandro Salazar <ale_salazar@outlook.com>
2022-03-21 23:20:46 -0400

    improve checkInvariants

--- wordle/src/AA.hs
+++ wordle/src/AA.hs
@@ -8,6 +8,7 @@ module AA (
 
 import Prelude hiding (lookup)
 import Data.Bifunctor ( Bifunctor(bimap) )
+import Data.Either (isRight, lefts, fromLeft)
 
 data AA k a = Empty
             | Node { lvl :: Int
@@ -70,28 +71,52 @@ lookup k (Node _ k' v lt rt)
   | k <  k' = lookup k lt
   | k  > k' = lookup k rt
 
-checkInvariants :: AA k a -> (Bool, [ AA k a ])
-checkInvariants Empty = (True, [])
-checkInvariants a = if all ($a) [leafLevel, leftChildLevel, rightGrandChildLevel, twoChilds]
-                    then bimap (fst left &&) (snd left ++) right
-                    else (False, a : snd left ++ snd right)
-  where
-    left = checkInvariants (lAA a)
-    right = checkInvariants (rAA a)
-    leafLevel :: AA k a -> Bool
-    leafLevel (Node l _ _ Empty Empty) = l == 1
-    leafLevel _ = True
-    leftChildLevel :: AA k a -> Bool
-    leftChildLevel Empty = True
-    leftChildLevel (Node _ _ _ Empty _) = True
-    leftChildLevel (Node l _ _ lt _) = lvl lt < l
-    rightGrandChildLevel :: AA k a -> Bool
-    rightGrandChildLevel Empty = True
-    rightGrandChildLevel (Node _ _ _ _ Empty) = True
-    rightGrandChildLevel (Node _ _ _ _ (Node _ _ _ _ Empty)) = True
-    rightGrandChildLevel (Node l _ _ _ rt) = lvl (rAA rt) < l
-    twoChilds :: AA k a -> Bool
-    twoChilds (Node l _ _ _ Empty) | l > 1 = False
-    twoChilds (Node l _ _ Empty _) | l > 1 = False
-    twoChilds _ = True
+newtype ValidAA k a = Valid (AA k a) deriving Show
 
+data InvariantError k a = WrongLeafLevel (AA k a)
+                        | WrongLeftChildLevel (AA k a)
+                        | WrongRightChildLevel (AA k a)
+                        | WrongRightGrandchildLevel (AA k a)
+                        | WrongChildCount (AA k a)
+                        deriving Show
+
+type AAValidity k a = Either (InvariantError k a) (ValidAA k a)
+
+checkInvariants :: AA k a -> Either [InvariantError k a] (ValidAA k a)
+checkInvariants Empty = Right $ Valid Empty
+checkInvariants n = if all isRight errors && all isRight childErrors
+                  then Right $ Valid n
+                  else Left $ foldMap (fromLeft []) childErrors ++ lefts errors
+  where
+    errors = map ($n) [leafLevel, leftChildLevel, rightChildLevel, rightGrandchildLevel, twoChilds]
+    leftTree = checkInvariants (lAA n)
+    rightTree = checkInvariants (rAA n)
+    childErrors = [leftTree, rightTree]
+    leafLevel :: AA k a -> AAValidity k a
+    leafLevel n@(Node l _ _ Empty Empty) = if l == 1
+                                         then Right $ Valid n
+                                         else Left $ WrongLeafLevel n
+    leafLevel n = Right $ Valid n
+    leftChildLevel :: AA k a -> AAValidity k a
+    leftChildLevel Empty = Right $ Valid Empty
+    leftChildLevel n@(Node _ _ _ Empty _) = Right $ Valid Empty
+    leftChildLevel n@(Node l _ _ lt _) = if lvl lt < l
+                                       then Right $ Valid n
+                                       else Left $ WrongLeftChildLevel n
+    rightChildLevel :: AA k a -> AAValidity k a
+    rightChildLevel Empty = Right $ Valid Empty
+    rightChildLevel n@(Node _ _ _ _ Empty) = Right $ Valid Empty
+    rightChildLevel n@(Node l _ _ _ lt) = if lvl lt <= l
+                                        then Right $ Valid n
+                                        else Left $ WrongRightChildLevel n
+    rightGrandchildLevel :: AA k a -> AAValidity k a
+    rightGrandchildLevel Empty = Right $ Valid Empty
+    rightGrandchildLevel n@(Node _ _ _ _ Empty) = Right $ Valid n
+    rightGrandchildLevel n@(Node _ _ _ _ (Node _ _ _ _ Empty)) = Right $ Valid n
+    rightGrandchildLevel n@(Node l _ _ _ rt) = if lvl (rAA rt) < l
+                                             then Right $ Valid n
+                                             else Left $ WrongRightGrandchildLevel n
+    twoChilds :: AA k a -> AAValidity k a
+    twoChilds n@(Node l _ _ _ Empty) | l > 1 = Left $ WrongChildCount n
+    twoChilds n@(Node l _ _ Empty _) | l > 1 = Left $ WrongChildCount n
+    twoChilds n = Right $ Valid n
