369511e898ad3db6ad384293d8c5f69b910b96e6
inojosacs <cristianinojosa@gmail.com>
2022-03-23 01:33:35 -0400

    pickTarget

--- wordle/src/Play.hs
+++ wordle/src/Play.hs
@@ -3,6 +3,8 @@ module Play where
 import qualified AA as AA
 import Util
 import Match
+import System.Random 
+import Control.Monad
 
 data GameState = GS { played :: Int
                     , won :: Int
@@ -15,6 +17,16 @@ initialState :: IO GameState
 initialState = do
   dict <- loadDictionary dictionary
   return $ GS 0 0 0 (Target "") dict
+
+
+pickTarget :: AA.AA String String -> IO Target
+pickTarget dict = do str <- reservoirSampling dict
+                     return $ Target str
+
+reservoirSampling :: AA.AA String String -> IO String
+reservoirSampling dict = fmap snd $ foldM (\(sz, curr) new -> getWord sz new curr) (1,"") dict
+                          where getWord sz new curr = do r <- randomRIO (1, sz) :: IO Int
+                                                         return (sz+1, if r == 1 then new else curr)
 
 instance Show GameState where
   show (GS played won streak target dict) =
